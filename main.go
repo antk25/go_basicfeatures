c63cd441bc205d34b7da07725c4a9fdf480cc3fc
Anton Kunaev <kunaevanton25@gmail.com>
2024-02-01 22:41:04 +0300

    Using variables

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	const price float32 = 200.00
-	const tax float32 = 34.00
+	var price float32 = 275.00
+	var tax float32 = 27.50
+	fmt.Println(price + tax)
+	// Значение 300 можно присвоить переменной типа float потому что 300 - является нетипизированной константой и преобразуется
+	price = 300
 	fmt.Println(price + tax)
 }
